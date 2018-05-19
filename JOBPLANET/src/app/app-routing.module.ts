ff789077838629b07ba01113f0570ca99c945ef5
Dusica Tanaskovic <dusicatanaskovic@gmail.com>
2018-05-19 11:56:04 +0200

    superuser component-updating

--- JOBPLANET/src/app/app-routing.module.ts
+++ JOBPLANET/src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes = [
         path: 'superuser',
         component: SuperuserComponent,
         children: [
+
             {
                 path: 'email-form',
                 component:  EmailFormComponent 
--- JOBPLANET/src/app/superuser/superuser.component.ts
+++ JOBPLANET/src/app/superuser/superuser.component.ts
@@ -8,23 +8,32 @@ import { EmailFormComponent } from './email-form/email-form.component';
   styleUrls: ['./superuser.component.css']
 })
 
-  
+
 export class SuperuserComponent implements OnInit {
+
+  public options = ['Users', 'Jobs', 'Alerts', 'Send message']
+
+  public currentNav;
 
   constructor(private route: ActivatedRoute, private _router: Router) {
 
-   }
+  }
 
   ngOnInit() {
-  
+
   }
-  navigateTo(value) {
-    if (value) {
-      this._router.navigate(['/email-form'], { relativeTo: this.route });
-    }
+  navigateTo() {
+    // if (value) {
+      this._router.navigate(['email-form'], { relativeTo: this.route });
+    // }
     return false;
   }
 
+  onChange(e) {
+    console.log(e);
+    this.currentNav = e.target.value;
+    this.navigateTo();
   }
+}
 
 
